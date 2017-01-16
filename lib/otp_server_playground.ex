557b093a8083611b3d51023838ea245e46a832c5
James Doyley <jdoyley@gmail.com>
2017-01-16 17:28:10 +0000

    First commit

---
+++ lib/otp_server_playground.ex
@@ -0,0 +1,18 @@
+defmodule OtpServerPlayground do
+  @moduledoc """
+  Documentation for OtpServerPlayground.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OtpServerPlayground.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
